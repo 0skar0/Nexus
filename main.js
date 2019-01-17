5ba3463fda03df48afb9d83d74d304f158162900
Oskar Olsson <oskarolsson86@gmail.com>
2019-01-17 11:09:53 +0100

    Oops

---
+++ main.js
@@ -0,0 +1,29 @@
+function startButton() {
+  document.getElementsByClassName("header")[0].style.display = "block";
+  document.getElementsByTagName("section")[0].style.display = "flex";
+  document.getElementsByClassName("startscreen")[0].style.display = "none";
+  nameInput = prompt("Enter your username: ");
+  localStorage.userName = nameInput;
+}
+
+function mapFunction() {
+  document.getElementsByTagName("div")[0].style.display = "none";
+  document.getElementsByTagName("section")[0].style.display = "none";
+  localStorage.mapIcon = true;
+}
+
+function bowFunction() {
+  document.getElementsByTagName("div")[0].style.display = "none";
+  document.getElementsByTagName("section")[0].style.display = "none";
+}
+
+function wandFunction() {
+  document.getElementsByTagName("div")[0].style.display = "none";
+  document.getElementsByTagName("section")[0].style.display = "none";
+}
+
+function backFunction() {
+  document.getElementsByTagName("div")[0].style.display = "none";
+  document.getElementsByTagName("section")[0].style.display = "none";
+  localStorage.backIcon = true;
+}
